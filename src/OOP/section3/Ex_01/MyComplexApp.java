4750442b7bfe453efafb78d0ed19211beb093ac2
Phentm <www.mamamax99.yakovlev@gmail.com>
2025-07-02 19:44:02 +0300

    OOP exercise 3.1: solved simple solution for exercise

--- src/OOP/section3/Ex_01/MyComplexApp.java
+++ src/OOP/section3/Ex_01/MyComplexApp.java
@@ -18,14 +18,39 @@ public class MyComplexApp {
         tempValues = getInput(scnr, "Enter complex number 2 (real and imaginary part): ");
         complex2 = new MyComplex(tempValues[0], tempValues[1]);
         
-        System.out.println(complex1);
-        System.out.println(complex2);
+        showComplexInfo(complex1, 1);
+        
+        showComplexInfo(complex2, 2);
+        
+        System.out.println();
+        
+        if (complex1.equals(complex2))
+            System.out.println("(" + complex1 + ") IS equal to ("
+                    + complex2 + ")");
+        else
+            System.out.println("(" + complex1 + ") is NOT equal to ("
+                    + complex2 + ")");
+        
+        System.out.println("(" + complex1 + ") + (" + complex2 + ") = ("
+                + complex1.addNew(complex2) + ")");
+    }
+    
+    public static void showComplexInfo(MyComplex cmplx, int index) {
+        System.out.println("\nNumber " + index + " is: (" + cmplx + ")");
+        
+        if (cmplx.isReal())
+            System.out.println("(" + cmplx + ") IS a pure real number");
+        else if (cmplx.isImaginary())
+            System.out.println("(" + cmplx + ") IS a pure imaginary number");
+        else
+            System.out.println("(" + cmplx + ") is NOT a pure real number\n"
+                    + "(" + cmplx + ") is NOT a pure imaginary number");
         
     }
     
     public static double[] getInput(Scanner scnr, String prompt) {
         String inputStr;
-        String[] inputVal = new String[2];
+        String[] inputVal;
         double[] outputVal = new double[2];
         
         System.out.print(prompt);
@@ -35,11 +60,12 @@ public class MyComplexApp {
             
             if (inputStr.contains(" "))
                 inputVal = inputStr.split(" ");
+            else
+                inputVal = new String[0];
             
-            if (inputStr.contains(" ")
+            if (inputVal.length == 2
                     && isDouble(inputVal[0]) 
-                    && isDouble(inputVal[1])
-                    && (inputStr.indexOf(' ') == inputStr.lastIndexOf(' ')))
+                    && isDouble(inputVal[1]))
                 break;
             
             System.err.println("Invalid input, please try again.");
